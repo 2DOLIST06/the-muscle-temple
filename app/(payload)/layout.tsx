b35c4d1ba4763f897e38eeee41facb3721863ff7
2DOLIST06 <contact@2dolist.fr>
2026-05-19 22:25:16 +0200

    feat(cms): redesign payload admin interface styling

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -2,6 +2,7 @@ import type { ServerFunctionClient } from 'payload'
 
 import configPromise from '@payload-config'
 import '@payloadcms/next/css'
+import './payload-theme.css'
 import { RootLayout, handleServerFunctions } from '@payloadcms/next/layouts'
 
 import { importMap } from './cms/importMap'
