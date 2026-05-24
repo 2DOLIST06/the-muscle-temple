3ef7ce3e009dac8a454c7deee6d66f9973d71c04
2DOLIST06 <contact@2dolist.fr>
2026-05-24 13:48:31 +0200

    Remove Payload CMS integration and restore classic admin setup (#62)

--- app/AppLayoutBoundary.tsx
+++ app/AppLayoutBoundary.tsx
@@ -1,14 +1,7 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
 import { SiteShell } from '@/components/layout/SiteShell';
 
 export function AppLayoutBoundary({ children }: Readonly<{ children: React.ReactNode }>) {
-  const pathname = usePathname();
-
-  if (pathname?.startsWith('/cms')) {
-    return <>{children}</>;
-  }
-
   return <SiteShell>{children}</SiteShell>;
 }
