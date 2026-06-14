18dc1ff2a1614ec5dcfec4d2a1b9f1ff21a1a69f
2DOLIST06 <contact@2dolist.fr>
2026-06-14 13:51:07 +0200

    Revert "Preserve article headings during HTML cleanup (#86)"
    This reverts commit 9711d0bfdcde87789ad94d9069adb8c0e600aece.

--- components/blog/RichContentRenderer.tsx
+++ components/blog/RichContentRenderer.tsx
@@ -1,18 +1,13 @@
 import { addHeadingIds } from '@/lib/content/article-utils';
 
-const removeFiguresFromHeading = (headingHtml: string) => headingHtml.replace(/<figure[\s\S]*?<\/figure>/gi, '').trim();
-
-export const cleanRichHtml = (contentHtml: string) =>
+const cleanRichHtml = (contentHtml: string) =>
   addHeadingIds(
     contentHtml
       .replace(/<p(?:\s[^>]*)?>(\s|&nbsp;|<br\s*\/?>)*<\/p>/gi, '')
       .replace(/<h([2-4])(?:\s[^>]*)?>(\s|&nbsp;|<br\s*\/?>)*<\/h\1>/gi, '')
       .replace(/<h([2-4])\b([^>]*)>\s*<strong>([\s\S]*?)<\/strong>\s*<\/h\1>/gi, '<h$1$2>$3</h$1>')
       .replace(/<h([2-4])\b([^>]*)>\s*<h[1-6][^>]*>([\s\S]*?)<\/h[1-6]>\s*<\/h\1>/gi, '<h$1$2>$3</h$1>')
-      .replace(/<h([2-4])\b([^>]*)>([\s\S]*?<figure[\s\S]*?<\/figure>[\s\S]*?)<\/h\1>/gi, (_match, level: string, attrs: string, inner: string) => {
-        const headingInner = removeFiguresFromHeading(inner);
-        return headingInner ? `<h${level}${attrs}>${headingInner}</h${level}>` : '';
-      })
+      .replace(/<h([2-4])\b([^>]*)>\s*<figure[\s\S]*?<\/figure>\s*<\/h\1>/gi, '')
       .replace(/<blockquote(?:\s[^>]*)?>(\s|&nbsp;|<br\s*\/?>)*<\/blockquote>/gi, '')
       .replace(/<p(?:\s[^>]*)?>\s*(<figure[\s\S]*?<\/figure>)\s*<\/p>/gi, '$1')
       .replace(/<img\b(?![^>]*\balt=)/gi, '<img alt=""')
